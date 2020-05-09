88ce0e90a89d2f83b1b8b25ed692ee9d984b3f1a
coding_jackalope <coding.jackalope@gmail.com>
2020-05-09 11:04:31 -0700

    [API] Updated version to 0.6.4.

--- API.lua
+++ API.lua
@@ -203,7 +203,7 @@ local Slab = {}
 -- Slab version numbers.
 local Version_Major = 0
 local Version_Minor = 6
-local Version_Revision = 3
+local Version_Revision = 4
 
 local FrameNumber = 0
 local FrameStatHandle = nil
