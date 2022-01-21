48441f001acbd0a921747b48798f5e6fe4bd88fc
Brandon Blanker Lim-it <flamendless8@gmail.com>
2022-01-21 16:05:40 +0800

    [Keyboard] refactor and improvements. #127

--- Internal/Input/Keyboard.lua
+++ Internal/Input/Keyboard.lua
@@ -26,106 +26,91 @@ SOFTWARE.
 
 local insert = table.insert
 
-local Common = require(SLAB_PATH .. '.Internal.Input.Common')
-local Stats = require(SLAB_PATH .. '.Internal.Core.Stats')
-local Utility = require(SLAB_PATH .. '.Internal.Core.Utility')
+local Common = require(SLAB_PATH .. ".Internal.Input.Common")
+local Stats = require(SLAB_PATH .. ".Internal.Core.Stats")
+local Utility = require(SLAB_PATH .. ".Internal.Core.Utility")
 
 local Keyboard = {}
 
-local KeyPressedFn = nil
-local KeyReleasedFn = nil
-local Events = {}
-local Keys = {}
+local fn_keypressed, fn_keyreleased
+local events, keys = {}, {}
 
-local function PushEvent(Type, Key, Scancode, IsRepeat)
-	insert(Events, {
-		Type = Type,
-		Key = Key,
-		Scancode = Scancode,
-		IsRepeat = IsRepeat,
-		Frame = Stats.GetFrameNumber()
+local function PushEvent(ev_type, key, scancode, is_repeat)
+	insert(events, {
+		type = ev_type,
+		key = key,
+		scancode = scancode,
+		is_repeat = is_repeat,
+		frame = Stats.GetFrameNumber(),
 	})
 end
 
-local function OnKeyPressed(Key, Scancode, IsRepeat)
-	PushEvent(Common.Event.Pressed, Key, Scancode, IsRepeat)
-
-	if KeyPressedFn ~= nil then
-		KeyPressedFn(Key, Scancode, IsRepeat)
+local function OnKeyPressed(key, scancode, is_repeat)
+	PushEvent(Common.Event.Pressed, key, scancode, is_repeat)
+	if fn_keypressed then
+		fn_keypressed(key, scancode, is_repeat)
 	end
 end
 
-local function OnKeyReleased(Key, Scancode)
-	PushEvent(Common.Event.Released, Key, Scancode, false)
-
-	if KeyReleasedFn ~= nil then
-		KeyReleasedFn(Key, Scancode)
+local function OnKeyReleased(key, scancode)
+	PushEvent(Common.Event.Released, key, scancode, false)
+	if fn_keyreleased then
+		fn_keyreleased(key, scancode)
 	end
 end
 
 local function ProcessEvents()
-	Keys = {}
+	Utility.ClearTable(keys)
 
 	-- Soft keyboards found on mobile/tablet devices will push keypressed/keyreleased events when the user
 	-- releases from the pressed key. All released events pushed as the same frame as the pressed events will be
 	-- pushed to the events table for the next frame to process.
-	local NextEvents = {}
-
-	for I, V in ipairs(Events) do
-		if Keys[V.Scancode] == nil then
-			Keys[V.Scancode] = {}
+	local next_events = {}
+	for i, v in ipairs(events) do
+		if not keys[v.scancode] then
+			keys[v.scancode] = {}
 		end
+		local key = keys[v.scancode]
 
-		local Key = Keys[V.Scancode]
-
-		if Utility.IsMobile() and V.Type == Common.Event.Released and Key.Frame == V.Frame then
-			V.Frame = V.Frame + 1
-			insert(NextEvents, V)
+		if Utility.IsMobile() and v.type == Common.Event.Released and key.frame == v.frame then
+			v.frame = v.frame + 1
+			insert(next_events, v)
 		else
-			Key.Type = V.Type
-			Key.Key = V.Key
-			Key.Scancode = V.Scancode
-			Key.IsRepeat = V.IsRepeat
-			Key.Frame = V.Frame
+			key.type = v.type
+			key.key = v.key
+			key.scancode = v.scancode
+			key.is_repeat = v.is_repeat
+			key.frame = v.frame
 		end
 	end
-
-	Events = NextEvents
+	events = next_events
 end
 
-function Keyboard.Initialize(Args)
-	KeyPressedFn = love.handlers['keypressed']
-	KeyReleasedFn = love.handlers['keyreleased']
-	love.handlers['keypressed'] = OnKeyPressed
-	love.handlers['keyreleased'] = OnKeyReleased
+function Keyboard.Initialize(args)
+	fn_keypressed = love.handlers.keypressed
+	fn_keyreleased = love.handlers.keyreleased
+	love.handlers.keypressed = OnKeyPressed
+	love.handlers.keyreleased = OnKeyReleased
 end
 
 function Keyboard.Update()
 	ProcessEvents()
 end
 
-function Keyboard.IsPressed(Key)
-	local Item = Keys[Key]
-
-	if Item == nil then
-		return false
-	end
-
-	return Item.Type == Common.Event.Pressed
+function Keyboard.IsPressed(key)
+	local item = keys[key]
+	if not item then return false end
+	return item.type == Common.Event.Pressed
 end
 
-function Keyboard.IsReleased(Key)
-	local Item = Keys[Key]
-
-	if Item == nil then
-		return false
-	end
-
-	return Item.Type == Common.Event.Released
+function Keyboard.IsReleased(key)
+	local item = keys[key]
+	if not item then return false end
+	return item.type == Common.Event.Released
 end
 
-function Keyboard.IsDown(Key)
-	return love.keyboard.isScancodeDown(Key)
+function Keyboard.IsDown(key)
+	return love.keyboard.isScancodeDown(key)
 end
 
 return Keyboard
