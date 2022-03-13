bef15349f75667c644fa78e7e45f5dfd9ce86f93
Brandon Blanker Lim-it <flamendless8@gmail.com>
2022-03-13 12:41:45 +0800

    [ListBox] refactor + improvements. #127

--- Internal/UI/ListBox.lua
+++ Internal/UI/ListBox.lua
@@ -24,195 +24,175 @@ SOFTWARE.
 
 --]]
 
-local Cursor = require(SLAB_PATH .. '.Internal.Core.Cursor')
-local DrawCommands = require(SLAB_PATH .. '.Internal.Core.DrawCommands')
-local LayoutManager = require(SLAB_PATH .. '.Internal.UI.LayoutManager')
-local Mouse = require(SLAB_PATH .. '.Internal.Input.Mouse')
-local Region = require(SLAB_PATH .. '.Internal.UI.Region')
-local Stats = require(SLAB_PATH .. '.Internal.Core.Stats')
-local Style = require(SLAB_PATH .. '.Style')
-local Text = require(SLAB_PATH .. '.Internal.UI.Text')
-local Window = require(SLAB_PATH .. '.Internal.UI.Window')
+local format = string.format
+
+local Cursor = require(SLAB_PATH .. ".Internal.Core.Cursor")
+local DrawCommands = require(SLAB_PATH .. ".Internal.Core.DrawCommands")
+local LayoutManager = require(SLAB_PATH .. ".Internal.UI.LayoutManager")
+local Mouse = require(SLAB_PATH .. ".Internal.Input.Mouse")
+local Region = require(SLAB_PATH .. ".Internal.UI.Region")
+local Stats = require(SLAB_PATH .. ".Internal.Core.Stats")
+local Style = require(SLAB_PATH .. ".Style")
+local Utility = require(SLAB_PATH .. ".Internal.Core.Utility")
+local Window = require(SLAB_PATH .. ".Internal.UI.Window")
 
 local ListBox = {}
-local Instances = {}
-local ActiveInstance = nil
+local instances = {}
+local active
 
-local function GetItemInstance(Instance, Id)
-	if Instance ~= nil then
-		if Instance.Items[Id] == nil then
-			local Item = {}
-			Item.Id = Id
-			Item.X = 0.0
-			Item.Y = 0.0
-			Item.W = 0.0
-			Item.H = 0.0
-			Instance.Items[Id] = Item
-		end
-		return Instance.Items[Id]
+local function GetItemInstance(instance, id)
+	if not instance then return end
+	if not instance.Items[id] then
+		local item = {
+			Id = id,
+			X = 0, Y = 0,
+			W = 0, H = 0,
+		}
+		instance.Items[id] = item
 	end
-	return nil
+	return instance.Items[id]
 end
 
-local function GetInstance(Id)
-	if Instances[Id] == nil then
-		local Instance = {}
-		Instance.Id = Id
-		Instance.X = 0.0
-		Instance.Y = 0.0
-		Instance.W = 0.0
-		Instance.H = 0.0
-		Instance.Items = {}
-		Instance.ActiveItem = nil
-		Instance.HotItem = nil
-		Instance.Selected = false
-		Instance.StatHandle = nil
-		Instances[Id] = Instance
-	end
-	return Instances[Id]
+local function GetInstance(id)
+	if  instances[id] then return instances[id] end
+	local instance = {
+		Id = id,
+		X = 0, Y = 0,
+		W = 0, H = 0,
+		Items = {},
+		Selected = false
+	}
+	instances[id] = instance
+	return instance
 end
 
-function ListBox.Begin(Id, Options)
-	local StatHandle = Stats.Begin('ListBox', 'Slab')
+local TBL_DEF = {
+	W = 150,
+	H = 150,
+	Clear = false,
+	Rounding = Style.WindowRounding,
+	StretchW = false,
+	StretchH = false,
+}
+local TBL_IGNORE = {Ignore = true}
 
-	Options = Options == nil and {} or Options
-	Options.W = Options.W == nil and 150.0 or Options.W
-	Options.H = Options.H == nil and 150.0 or Options.H
-	Options.Clear = Options.Clear == nil and false or Options.Clear
-	Options.Rounding = Options.Rounding == nil and Style.WindowRounding or Options.Rounding
-	Options.StretchW = Options.StretchW or false
-	Options.StretchH = Options.StretchH or false
+function ListBox.Begin(id, opt)
+	local stat_handle = Stats.Begin("ListBox", "Slab")
+	opt = opt or TBL_DEF
+	opt.W = opt.W or TBL_DEF.W
+	opt.H = opt.H or TBL_DEF.H
+	opt.Clear = opt.Clear or TBL_DEF.Clear
+	opt.Rounding = opt.Rounding or TBL_DEF.Rounding
+	opt.StretchW = opt.StretchW or TBL_DEF.StretchW
+	opt.StretchH = opt.StretchH or TBL_DEF.StretchH
 
-	local Instance = GetInstance(Window.GetItemId(Id))
-	local W = Options.W
-	local H = Options.H
-
-	if Options.Clear then
-		Instance.Items = {}
+	local instance = GetInstance(Window.GetItemId(id))
+	if opt.Clear then
+		Utility.ClearTable(instance.Items)
 	end
-
-	W, H = LayoutManager.ComputeSize(W, H)
-	LayoutManager.AddControl(W, H, 'ListBox')
-
-	local RemainingW, RemainingH = Window.GetRemainingSize()
-	if Options.StretchW then
-		W = RemainingW
-	end
-
-	if Options.StretchH then
-		H = RemainingH
-	end
-
-	local X, Y = Cursor.GetPosition()
-	Instance.X = X
-	Instance.Y = Y
-	Instance.W = W
-	Instance.H = H
-	Instance.StatHandle = StatHandle
-	ActiveInstance = Instance
-
-	Cursor.SetItemBounds(X, Y, W, H)
-	Cursor.AdvanceY(0.0)
-
-	Window.AddItem(X, Y, W, H, Instance.Id)
-
-	local IsObstructed = Window.IsObstructedAtMouse()
-
-	local TX, TY = Window.TransformPoint(X, Y)
-	local MouseX, MouseY = Window.GetMousePosition()
-	Region.Begin(Instance.Id, {
-		X = X,
-		Y = Y,
-		W = W,
-		H = H,
-		SX = TX,
-		SY = TY,
+	local w, h = LayoutManager.ComputeSize(opt.W, opt.H)
+	LayoutManager.AddControl(w, h, "ListBox")
+	local rem_w, rem_h = Window.GetRemainingSize()
+	w = opt.StretchW and rem_w or w
+	h = opt.StretchH and rem_h or h
+	local x, y = Cursor.GetPosition()
+	instance.X = x
+	instance.Y = y
+	instance.W = w
+	instance.H = h
+	instance.StatHandle = stat_handle
+	active = instance
+	Cursor.SetItemBounds(x, y, w, h)
+	Cursor.AdvanceY(0)
+	Window.AddItem(x, y, w, h, instance.Id)
+	local is_obstructed = Window.IsObstructedAtMouse()
+	local tx, ty = Window.TransformPoint(x, y)
+	local mx, my = Window.GetMousePosition()
+	Region.Begin(instance.Id, {
+		X = x,
+		Y = y,
+		W = w,
+		H = h,
+		SX = tx,
+		SY = ty,
 		AutoSizeContent = true,
 		NoBackground = true,
 		Intersect = true,
-		MouseX = MouseX,
-		MouseY = MouseY,
+		MouseX = mx,
+		MouseY = my,
 		ResetContent = Window.HasResized(),
-		IsObstructed = IsObstructed,
-		Rounding = Options.Rounding
+		IsObstructed = is_obstructed,
+		Rounding = opt.Rounding
 	})
-
-	Instance.HotItem = nil
-	local InRegion = Region.Contains(MouseX, MouseY)
-	MouseX, MouseY = Region.InverseTransform(Instance.Id, MouseX, MouseY)
-	for K, V in pairs(Instance.Items) do
-		if not IsObstructed
-			and not Region.IsHoverScrollBar(Instance.Id)
-			and V.X <= MouseX and MouseX <= V.X + Instance.W and V.Y <= MouseY and MouseY <= V.Y + V.H
-			and InRegion
-			then
-			Instance.HotItem = V
+	instance.HotItem = nil
+	local in_region = Region.Contains(mx, my)
+	mx, my = Region.InverseTransform(instance.Id, mx, my)
+	for _, v in pairs(instance.Items) do
+		if not is_obstructed
+			and not Region.IsHoverScrollBar(instance.Id)
+			and v.X <= mx and mx <= v.X + instance.W and v.Y <= my and my <= v.Y + v.H
+			and in_region then
+			instance.HotItem = v
 		end
 
-		if Instance.HotItem == V or V.Selected then
-			DrawCommands.Rectangle('fill', V.X, V.Y, Instance.W, V.H, Style.TextHoverBgColor)
+		if instance.HotItem == v or v.Selected then
+			DrawCommands.Rectangle("fill", v.X, v.Y, instance.W, v.H, Style.TextHoverBgColor)
 		end
 	end
 
-	LayoutManager.Begin('Ignore', {Ignore = true})
+	LayoutManager.Begin("Ignore", TBL_IGNORE)
 end
 
-function ListBox.BeginItem(Id, Options)
-	Options = Options == nil and {} or Options
-	Options.Selected = Options.Selected == nil and false or Options.Selected
-
-	assert(ActiveInstance ~= nil, "Trying to call BeginListBoxItem outside of BeginListBox.")
-	assert(ActiveInstance.ActiveItem == nil,
-		"BeginListBoxItem was called for item '" .. (ActiveInstance.ActiveItem ~= nil and ActiveInstance.ActiveItem.Id or "nil") ..
-			"' without a call to EndListBoxItem.")
-	local Item = GetItemInstance(ActiveInstance, Id)
-	Item.X = ActiveInstance.X
-	Item.Y = Cursor.GetY()
-	Cursor.SetX(Item.X)
+local err_nil_active = "Trying to call BeginListBoxItem outside of BeginListBox."
+local err_nil_active_item = "Begin was called for item '%s' without a call to EndListBoxItem"
+function ListBox.BeginItem(id, opt)
+	assert(active, err_nil_active)
+	if not active.ActiveItem then error(format(err_nil_active_item, active.ActiveItem or "nil")) end
+	local item = GetItemInstance(active, id)
+	item.X = active.X
+	item.Y = Cursor.GetY()
+	Cursor.SetX(item.X)
 	Cursor.AdvanceX(0.0)
-	ActiveInstance.ActiveItem = Item
-	ActiveInstance.ActiveItem.Selected = Options.Selected
+	active.ActiveItem = item
+	active.ActiveItem.Selected = not not (opt and opt.Selected) --defaulf is false
 end
 
-function ListBox.IsItemClicked(Button, IsDoubleClick)
-	assert(ActiveInstance ~= nil, "Trying to call IsItemClicked outside of BeginListBox.")
-	assert(ActiveInstance.ActiveItem ~= nil, "IsItemClicked was called outside of BeginListBoxItem.")
-	if ActiveInstance.HotItem == ActiveInstance.ActiveItem then
-		Button = Button == nil and 1 or Button
-		if IsDoubleClick then
-			return Mouse.IsDoubleClicked(Button)
-		else
-			return Mouse.IsClicked(Button)
-		end
-	end
-	return false
+local err_item_clicked = "Trying to call IsItemClicked outside of BeginListBox."
+local err_item_clicked2 = "IsItemClicked was called outside of BeginListBoxItem."
+function ListBox.IsItemClicked(btn, is_double_clicked)
+	assert(active ~= nil, err_item_clicked)
+	assert(active.ActiveItem, err_item_clicked2)
+	if active.ActiveItem ~= active.ActiveItem then return false end
+	btn = btn or 1
+	if is_double_clicked then return Mouse.IsDoubleClicked(btn) end
+	return Mouse.IsClicked(btn)
 end
 
+local err_end = "Trying to call BeginListBoxItem outside of BeginListBox."
+local err_end2 = "Trying to call BeginListBoxItem outside of BeginListBox."
 function ListBox.EndItem()
-	assert(ActiveInstance ~= nil, "Trying to call BeginListBoxItem outside of BeginListBox.")
-	assert(ActiveInstance.ActiveItem ~= nil, "Trying to call EndListBoxItem without calling BeginListBoxItem.")
-	local ItemX, ItemY, ItemW, ItemH = Cursor.GetItemBounds()
-	ActiveInstance.ActiveItem.W = ItemW
-	ActiveInstance.ActiveItem.H = Cursor.GetLineHeight()
-	Cursor.SetY(ActiveInstance.ActiveItem.Y + ActiveInstance.ActiveItem.H)
-	Cursor.AdvanceY(0.0)
-	ActiveInstance.ActiveItem = nil
+	assert(active, err_end)
+	assert(active.ActiveItem, err_end2)
+	local w = select(3, Cursor.GetItemBounds())
+	local item = active.ActiveItem
+	item.W = w
+	item.H = Cursor.GetLineHeight()
+	Cursor.SetY(item.Y + item.H)
+	Cursor.AdvanceY(0)
+	active.ActiveItem = nil
 end
 
+local err_end3 = "EndListBox was called without calling BeginListBox."
 function ListBox.End()
-	assert(ActiveInstance ~= nil, "EndListBox was called without calling BeginListBox.")
+	assert(active, err_end3)
 	Region.End()
 	Region.ApplyScissor()
-
-	Cursor.SetItemBounds(ActiveInstance.X, ActiveInstance.Y, ActiveInstance.W, ActiveInstance.H)
-	Cursor.SetPosition(ActiveInstance.X, ActiveInstance.Y)
-	Cursor.AdvanceY(ActiveInstance.H)
-
+	Cursor.SetItemBounds(active.X, active.Y, active.W, active.H)
+	Cursor.SetPosition(active.X, active.Y)
+	Cursor.AdvanceY(active.H)
 	LayoutManager.End()
-
-	Stats.End(ActiveInstance.StatHandle)
-
-	ActiveInstance = nil
+	Stats.End(active.StatHandle)
+	active = nil
 end
 
 return ListBox
